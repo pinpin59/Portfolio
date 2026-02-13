672548fded0e4381ca3b7402489372c068ec0ae2
Kevin Pintar <kevinpintar@hotmail.fr>
2026-02-13 23:44:38 +0100

    add lazy loading router

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,15 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./scenes/Home/Home";
-import Projects from "./scenes/Projects/Projects";
-import ProjectDetail from "./scenes/Projects/ProjectDetail";
+import React, { Suspense } from "react";
+
+// Lazy loading des pages
+const Home = React.lazy(() => import("./scenes/Home/Home"));
+const Projects = React.lazy(() => import("./scenes/Projects/Projects"));
+const ProjectDetail = React.lazy(
+  () => import("./scenes/Projects/ProjectDetail"),
+);
 
 const AppRouter = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/projects/:slug" element={<ProjectDetail />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:slug" element={<ProjectDetail />} />
+      </Routes>
+    </Suspense>
   );
 };
 
